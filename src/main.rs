7f74e581657fc72e46be51d1a63459ecfaaf055b
Casper <89966510+CasperCal@users.noreply.github.com>
2022-10-02 20:20:34 +0200

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use blockchainlib::*;
+
+
+fn main() {
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned(), 0x000000ffffffffffffffffffffffffff);
+
+    block.hash = block.hash();
+
+    println!("{:?}", &block);
+
+    block.mine();
+
+    println!("{:?}", &block);
+
+}
